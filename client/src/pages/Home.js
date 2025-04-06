9248eb22874bca9084c79adcc842e9ee6a75e569
acecode101 <dharshinibaskaran20@gmail.com>
2025-04-07 02:03:16 +0530

    Update Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Home = () => {
   return (
     <div className="container">
-      <h1>Welcome to MERN Wallet</h1>
+      <h1>Welcome to FASTPAY Wallet</h1>
       <div>
         <Link to="/signup">Sign Up</Link> | <Link to="/login">Login</Link>
       </div>
