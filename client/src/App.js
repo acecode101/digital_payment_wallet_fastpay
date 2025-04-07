764e5e876117faab51103fe1e1972602d0edc86a
acecode101 <dharshinibaskaran20@gmail.com>
2025-04-07 18:27:41 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,23 @@
-// client/src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import Graph from './pages/Graph';
 
-const App = () => {
+function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Home />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/graph" element={<Graph />} />
       </Routes>
     </Router>
   );
-};
+}
 
 export default App;
