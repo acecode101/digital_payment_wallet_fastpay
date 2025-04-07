9063c74b69a5c71fe588c36502759e8989dce6de
acecode101 <dharshinibaskaran20@gmail.com>
2025-04-07 18:04:27 +0530

    Update Transaction.js

--- server/models/Transaction.js
+++ server/models/Transaction.js
@@ -1,24 +1,10 @@
 const mongoose = require('mongoose');
 
-const transactionSchema = new mongoose.Schema({
-  sender: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
-  },
-  receiver: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
-  },
-  amount: {
-    type: Number,
-    required: true,
-  },
-  timestamp: {
-    type: Date,
-    default: Date.now,
-  },
+const TransactionSchema = new mongoose.Schema({
+  userId: String,
+  type: String,
+  amount: Number,
+  date: { type: Date, default: Date.now }
 });
 
-module.exports = mongoose.model('Transaction', transactionSchema);
+module.exports = mongoose.model('Transaction', TransactionSchema);
